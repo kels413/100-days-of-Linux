ec8f42d043a41794d915ad85de036e65a8ef8d1f
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-27 19:08:20 +0100

    compare two numbers

---
+++ shell-scripting/conditionals/5-task.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+<<COMMENT
+Write a script that compares two numbers entered by the user and prints the larger of the two.
+COMMENT
+
+echo -n "please Enter Num: "
+read num1
+echo -n "please Enter Num2: "
+read num2
+
+if (($num))

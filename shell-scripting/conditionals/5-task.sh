0d72563f49f3d22db20f070046d75e5b594a646d
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-27 19:15:40 +0100

    check for larger number

--- shell-scripting/conditionals/5-task.sh
+++ shell-scripting/conditionals/5-task.sh
@@ -9,4 +9,12 @@ read num1
 echo -n "please Enter Num2: "
 read num2
 
-if (($num))
+if [ $num1 -gt $num2 ]; then
+#if ((num1 > num2 )); then
+    echo "$num1 is greater than $num2"
+elif ((num2 > num1)); then
+    echo "$num2 is greater than $num1"
+else
+    echo "the are the same"
+fi
+

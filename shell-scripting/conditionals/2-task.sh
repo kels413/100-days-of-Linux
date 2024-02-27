c4f1e32f041ee3b15448a5e5afabf814fddd9ba0
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-27 18:32:39 +0100

    check for file

---
+++ shell-scripting/conditionals/2-task.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+cat << COMMENT 
+Create a script that checks if a file named "example.txt" exists in the current directory. If it does, print "File exists," otherwise, print "File not found."
+COMMENT

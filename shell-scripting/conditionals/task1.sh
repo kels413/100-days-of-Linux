430bf8151ae4111c14ba7f10f8d95815025b035d
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-27 18:08:35 +0100

    created task1

--- shell-scripting/conditionals/task1.sh
+++ shell-scripting/conditionals/task1.sh
@@ -4,3 +4,11 @@
 Write a Bash script that takes a number as a command-line argument and prints "Even" if the number is even and "Odd" if it's odd.
 COMMENT
 
+
+num=$1
+
+if [ "$num" / 2 -eq 0]
+then
+
+else
+fi

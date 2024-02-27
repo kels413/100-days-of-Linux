b1491cd20d08ef48e1bceebcbd0d0ada42ee0630
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-27 18:35:42 +0100

    updated 2-task2

--- shell-scripting/conditionals/2-task.sh
+++ shell-scripting/conditionals/2-task.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
-cat << COMMENT 
+<<COMMENT 
 Create a script that checks if a file named "example.txt" exists in the current directory. If it does, print "File exists," otherwise, print "File not found."
 COMMENT
+
+
+file='example.txt'
+
+if [ -f $file ]; then
+    echo "$file exists"
+else
+    echo "$file not found"
+fi

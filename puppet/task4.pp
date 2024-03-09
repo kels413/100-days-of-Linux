9cfe79f8a51102e2e24ddb2220af6063462fe2a7
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-03-09 21:09:32 +0100

    updated task4

---
+++ puppet/task4.pp
@@ -0,0 +1,10 @@
+/*
+Parameterize Content:
+
+Extend the manifest to allow the content of the file to be specified as a parameter. This way, you can easily change the content without modifying the manifest.
+*/
+
+
+file{
+    ensure => present,
+}

db684651c7c1d067d6cc5cab73e316ff5eacba3d
kelvin okoye <kelvinokoye413@gmail.com>
2024-03-09 05:06:07 +0000

    file ownership

--- puppet/task1.pp
+++ puppet/task1.pp
@@ -1,6 +1,10 @@
+# File Ownership:
+# Modify the Puppet manifest to ensure that the file /home/vagrant/file.txt is owned by the user vagrant and # the group vagrant.
+
+
 file {'/home/vagrant/file.txt':
     ensure => present,
-    owner => vagrant,
-    group => vagrant,
+    owner => 'vagrant',
+    group => 'vagrant',
     content => 'I am owned by kelly ayo',
 }

1a3962c70cb9fefd0d4a313ff1c15709f05c78b0
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-03-09 21:08:43 +0100

    updated main.pp

--- puppet/main.pp
+++ puppet/main.pp
@@ -1,6 +1,11 @@
 things to learn
 
+what is configuration management? 
 what is pupet
 what is pupet bot
 what is pupet manifest
 what is pupet lab
+
+
+configuration management is the process of automating, the process of configuration,
+deployment and maintanance of software and IT Infracstructure in a systematic way.
